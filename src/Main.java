88d880c701d35a40091a1bc01a22381c012e2c17
vkuna <vkunavin@mail.ru>
2023-10-16 00:04:20 +0300

    1 of 4;

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,36 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        String inputFName="Input.txt";
-        Scanner sq= new Scanner(new File("Input.txt"));
-        int n=sq.nextInt();
-        int m=sq.nextInt();
-        int k= sq.nextInt();
-
-        String result;
-        int t=0;// cutsCount
-      if(n<=k) {
-          result = "NO";
-          System.out.println(result);
-
-      }
-       else
-        {
-           while (m>0)
-           {
-               m=m-n+k;
-               t++;
-           }
-
+    public static void main(String[] args) throws IOException {
+        String inputFName = "Input.txt";
+        Scanner sq = new Scanner(new File("Input.txt"));
+        int n = sq.nextInt();
+        int m = sq.nextInt();
+        int k = sq.nextInt();
+        String f;
+        if (k >= n) {
+            f = "NO";
+            String OutPutFName = "Output.txt";
+            Files.writeString(Path.of(OutPutFName), f);
         }
-        System.out.println(t);
+        else {
+            int t = 0;
+            while (m > 0) {
+                m = m - n + k;
+                t++;
+            }
 
-
+            String OutPutFName = "Output.txt";
+            String s=Integer.toString(t);
+            Files.writeString(Path.of(OutPutFName),s);
+        }
     }
 }
